8ccce82bbbbf5269cd58b5d95f5b70e0ee503c50
gordonstephen <12711372+gordonstephen@users.noreply.github.com>
2018-09-27 19:23:25 +0100

    - NEW: Started using es6modules for the es6 classes. - NEW: Now use rollup to bundle our modules into a single js file.

---
+++ src/colorstate.js
@@ -0,0 +1,5 @@
+export class ColorState {
+    constructor (buffer) {
+        this.buffer = buffer;
+    }
+};
---
+++ src/nucleotide.js
@@ -0,0 +1,7 @@
+export class Nucleotide {
+    constructor(allele, colorLight, colorDark) {
+        this.allele = allele;
+        this.colorLight = colorLight;
+        this.colorDark = colorDark;
+    }
+};
