1f94361a33b4a7875f9e014065d897740bca721c
gordonstephen <12711372+gordonstephen@users.noreply.github.com>
2019-11-15 15:07:01 +0000

    Fix multi-chromosome rendering bug
    Fixed issue which saw maps being rendered inconsistently when viewed
    with the genotypes which should be directly underneath them.

--- src/genotypecanvas.js
+++ src/genotypecanvas.js
@@ -13,17 +13,21 @@ export default class GenotypeCanvas {
     this.backContext = this.backBuffer.getContext('2d');
     this.mapCanvasHeight = 40;
     this.lineNamesWidth = 100;
-    this.alleleCanvasWidth = width - this.lineNamesWidth;
-    this.alleleCanvasHeight = height - this.mapCanvasHeight;
+
+    this.scrollbarWidth = 10;
+    this.scrollbarHeight = 10;
+
+    this.alleleCanvasWidth = width - this.lineNamesWidth - this.scrollbarWidth;
+    this.alleleCanvasHeight = height - this.mapCanvasHeight - this.scrollbarHeight;
     this.backContext.lineWidth = 1;
 
     this.boxSize = boxSize;
     this.fontSize = fontSize;
 
-    this.verticalScrollbar = new ScrollBar(width, this.alleleCanvasHeight - 10,
-      10, this.alleleCanvasHeight - 10, true);
-    this.horizontalScrollbar = new ScrollBar(width - this.lineNamesWidth - 10 - 1,
-      height, width - this.lineNamesWidth - 10 - 1, 10, false);
+    this.verticalScrollbar = new ScrollBar(width, this.alleleCanvasHeight + this.scrollbarHeight,
+      this.scrollbarWidth, this.alleleCanvasHeight, true);
+    this.horizontalScrollbar = new ScrollBar(width - this.lineNamesWidth + this.scrollbarWidth - 1,
+      height, width - this.lineNamesWidth - 1, this.scrollbarHeight, false);
 
     this.translatedX = 0;
     this.translatedY = 0;
@@ -53,10 +57,11 @@ export default class GenotypeCanvas {
     this.drawingContext.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
     if (this.redraw) {
+      const dataWidth = Math.floor(this.alleleCanvasWidth / this.boxSize);
+
       const markerStart = Math.floor(this.translatedX / this.boxSize);
-      const markerEnd = Math.min(Math.ceil((this.translatedX + this.alleleCanvasWidth) / this.boxSize), this.dataSet.markerCount());
+      const markerEnd = Math.min(markerStart + dataWidth, this.dataSet.markerCount());
       const markerData = this.dataSet.mapDataFor(markerStart, markerEnd);
-      const dataWidth = markerEnd - markerStart;
 
       const germplasmStart = Math.floor(this.translatedY / this.boxSize);
       const germplasmEnd = Math.min(germplasmStart + Math.floor(this.canvas.height / this.boxSize), this.dataSet.lineCount());
@@ -141,7 +146,7 @@ export default class GenotypeCanvas {
     markerData.forEach((chromosome) => {
       const { markers } = chromosome;
       // Draw a rectangle outline for the map
-      const canvasW = this.alleleCanvasWidth - this.verticalScrollbar.width;
+      const canvasW = this.alleleCanvasWidth;
       if (cumulativeTranslation < canvasW) {
         let mapWidth = Math.floor(canvasW * (markers.length / dataWidth));
         if (cumulativeTranslation + mapWidth > canvasW) {
@@ -151,8 +156,8 @@ export default class GenotypeCanvas {
 
         this.renderMarkers(markers, mapWidth);
 
-        cumulativeTranslation += mapWidth + 20;
-        this.backContext.translate(mapWidth + 20, 0);
+        cumulativeTranslation += mapWidth + 50;
+        this.backContext.translate(mapWidth + 50, 0);
       }
     });
 
@@ -185,7 +190,7 @@ export default class GenotypeCanvas {
       for (let chromosome = 0; chromosome < data.length; chromosome += 1) {
         const { genotypes } = data[chromosome];
          // Draw a rectangle outline for the map
-        const canvasW = this.alleleCanvasWidth - this.verticalScrollbar.width;
+        const canvasW = this.alleleCanvasWidth;
         if (cumulativeTranslation < canvasW) {
           let mapWidth = Math.floor(canvasW * (genotypes.length / (markerEnd - markerStart)));
           if (cumulativeTranslation + mapWidth > canvasW) {
@@ -194,8 +199,8 @@ export default class GenotypeCanvas {
           for (let genotype = 0; genotype < genotypes.length; genotype += 1) {
             this.backContext.drawImage(this.colorStamps[genotypes[genotype]], (genotype * this.boxSize), (germplasm * this.boxSize));
           }
-          cumulativeTranslation += mapWidth + 20;
-          this.backContext.translate(mapWidth + 20, 0);
+          cumulativeTranslation += mapWidth + 50;
+          this.backContext.translate(mapWidth + 50, 0);
         }
       }
       this.backContext.restore();
@@ -217,16 +222,16 @@ export default class GenotypeCanvas {
     this.backContext.save();
     this.backContext.translate(this.lineNamesWidth, this.mapCanvasHeight);
     this.backContext.fillStyle = '#aaa';
-    this.backContext.strokeRect(this.alleleCanvasWidth - 10, this.alleleCanvasHeight - 10, 10, 10);
-    this.backContext.fillRect(this.alleleCanvasWidth - 10, this.alleleCanvasHeight - 10, 10, 10);
+    this.backContext.strokeRect(this.alleleCanvasWidth, this.alleleCanvasHeight - this.scrollbarHeight, this.scrollbarWidth, this.scrollbarHeight);
+    this.backContext.fillRect(this.alleleCanvasWidth, this.alleleCanvasHeight - this.scrollbarHeight, this.scrollbarWidth, this.scrollbarHeight);
     this.backContext.restore();
   }
 
-  move(diffX, diffY) {
-    const mapToRange = (num, inMin, inMax, outMin, outMax) => {
-      return (num - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
-    };
+  mapToRange(num, inMin, inMax, outMin, outMax) {
+    return (num - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+  };
 
+  move(diffX, diffY) {
     if (this.maxCanvasWidth > this.canvas.width) {
       this.translatedX -= diffX;
       if (this.translatedX < 0) { this.translatedX = 0; }
@@ -240,7 +245,7 @@ export default class GenotypeCanvas {
       // console.log(foundChroms);
 
       const scrollWidth = this.alleleCanvasWidth - 10 - 20;
-      const scrollX = Math.floor(mapToRange(this.translatedX, 0, this.maxCanvasWidth - this.alleleCanvasWidth, 0, scrollWidth));
+      const scrollX = Math.floor(this.mapToRange(this.translatedX, 0, this.maxCanvasWidth - this.alleleCanvasWidth, 0, scrollWidth));
       this.horizontalScrollbar.move(scrollX, this.horizontalScrollbar.y);
     }
 
@@ -250,7 +255,7 @@ export default class GenotypeCanvas {
       if ((this.translatedY / this.boxSize) >= ((this.maxCanvasHeight / this.boxSize) - (this.alleleCanvasHeight / this.boxSize))) { this.translatedY = this.maxCanvasHeight - this.alleleCanvasHeight; }
 
       const scrollHeight = this.alleleCanvasHeight - 10 - 20;
-      const scrollY = Math.floor(mapToRange(this.translatedY, 0, this.maxCanvasHeight - this.alleleCanvasHeight, 0, scrollHeight));
+      const scrollY = Math.floor(this.mapToRange(this.translatedY, 0, this.maxCanvasHeight - this.alleleCanvasHeight, 0, scrollHeight));
       this.verticalScrollbar.move(this.verticalScrollbar.x, scrollY);
     }
 
@@ -280,24 +285,24 @@ export default class GenotypeCanvas {
     this.prerender();
   }
 
-  rainbowColor(numOfSteps, step) {
-    // This function generates vibrant, "evenly spaced" colours (i.e. no clustering). This is ideal for creating easily distinguishable vibrant markers in Google Maps and other apps.
-    // Adam Cole, 2011-Sept-14
-    // HSV to RBG adapted from: http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
-    let r, g, b;
-    const h = step / numOfSteps;
-    const i = ~~(h * 6);
-    const f = h * 6 - i;
-    const q = 1 - f;
-    switch(i % 6){
-        case 0: r = 1; g = f; b = 0; break;
-        case 1: r = q; g = 1; b = 0; break;
-        case 2: r = 0; g = 1; b = f; break;
-        case 3: r = 0; g = q; b = 1; break;
-        case 4: r = f; g = 0; b = 1; break;
-        case 5: r = 1; g = 0; b = q; break;
-    }
-    let c = "#" + ("00" + (~ ~(r * 255)).toString(16)).slice(-2) + ("00" + (~ ~(g * 255)).toString(16)).slice(-2) + ("00" + (~ ~(b * 255)).toString(16)).slice(-2);
-    return (c);
-}
+//   rainbowColor(numOfSteps, step) {
+//     // This function generates vibrant, "evenly spaced" colours (i.e. no clustering). This is ideal for creating easily distinguishable vibrant markers in Google Maps and other apps.
+//     // Adam Cole, 2011-Sept-14
+//     // HSV to RBG adapted from: http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
+//     let r, g, b;
+//     const h = step / numOfSteps;
+//     const i = ~~(h * 6);
+//     const f = h * 6 - i;
+//     const q = 1 - f;
+//     switch(i % 6){
+//         case 0: r = 1; g = f; b = 0; break;
+//         case 1: r = q; g = 1; b = 0; break;
+//         case 2: r = 0; g = 1; b = f; break;
+//         case 3: r = 0; g = q; b = 1; break;
+//         case 4: r = f; g = 0; b = 1; break;
+//         case 5: r = 1; g = 0; b = q; break;
+//     }
+//     let c = "#" + ("00" + (~ ~(r * 255)).toString(16)).slice(-2) + ("00" + (~ ~(g * 255)).toString(16)).slice(-2) + ("00" + (~ ~(b * 255)).toString(16)).slice(-2);
+//     return (c);
+// }
 }
