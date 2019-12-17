54c0afdfdb9396c02a9f5915bdbb69482b4e0fb4
gordonstephen <12711372+gordonstephen@users.noreply.github.com>
2019-12-17 17:13:33 +0000

    Changes required to include axios as a library
    This was tricky as axios behaves a little differently from other
    libraries.

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
+import json from '@rollup/plugin-json';
 
 export default {
   input: 'src/flapjack-bytes.js',
@@ -11,8 +12,8 @@ export default {
   },
 
   plugins: [
-    nodeResolve(),
-
+    json(),
+    nodeResolve({ jsnext: true, preferBuiltins: true, browser: true }),
     babel({
       exclude: 'node_modules/**',
     }),
