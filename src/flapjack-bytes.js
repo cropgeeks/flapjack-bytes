2bd1efc8aa3666d4ae82ebc818bda091cbdc2b5d
Guilhem Sempéré <guilhem.sempere@cirad.fr>
2025-09-26 11:34:22 +0200

    Adjusted zoom range

--- src/flapjack-bytes.js
+++ src/flapjack-bytes.js
@@ -437,8 +437,8 @@ export default function GenotypeRenderer() {
     const range = document.createElement('input');
     range.id = 'zoom-control';
     range.setAttribute('type', 'range');
-    range.min = 2;
-    range.max = 64;
+    range.min = 1;
+    range.max = 32;
     range.value = boxSize;
 	range.style.width = "250px";
 	
