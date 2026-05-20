c21d4008d4105f6b18023941f641b71674f929c3
Guilhem Sempéré <guilhem.sempere@cirad.fr>
2026-05-20 17:52:06 +0200

    Made sure reference line is shown first when sorting by similarity

--- src/sort/SimilarityLineSort.js
+++ src/sort/SimilarityLineSort.js
@@ -1,6 +1,5 @@
 import {germplasmSimilarityScore} from '../Similarity'
 
-
 export default class SimilarityLineSort {
   constructor(referenceName, chromosomeIndices){
     this.referenceName = referenceName;
@@ -10,17 +9,42 @@ export default class SimilarityLineSort {
   }
 
   sort(dataSet){
-    const referenceIndex = dataSet.germplasmListFiltered.findIndex(germplasm => germplasm.name == this.referenceName);
+    const referenceIndex = dataSet.germplasmListFiltered.findIndex(
+      germplasm => germplasm.name == this.referenceName
+    );
 
     this.scoreMap = new Map();
+
     for (let comparedIndex in dataSet.germplasmListFiltered){
       this.scoreMap.set(
         dataSet.germplasmListFiltered[comparedIndex].name,
-        germplasmSimilarityScore(dataSet, referenceIndex, comparedIndex, this.chromosomeIndices),
+        germplasmSimilarityScore(
+          dataSet,
+          referenceIndex,
+          comparedIndex,
+          this.chromosomeIndices,
+        ),
       );
     }
 
-    dataSet.germplasmListFiltered.sort((a, b) => this.scoreMap.get(b.name) - this.scoreMap.get(a.name));
+    dataSet.germplasmListFiltered.sort((a, b) => {
+      const aIsReference = a.name === this.referenceName;
+      const bIsReference = b.name === this.referenceName;
+
+      // Force reference line to absolute top
+      if (aIsReference && !bIsReference){
+        return -1;
+      }
+
+      if (!aIsReference && bIsReference){
+        return 1;
+      }
+
+      const aScore = this.scoreMap.get(a.name);
+      const bScore = this.scoreMap.get(b.name);
+
+      return bScore - aScore;
+    });
   }
 
   getScore(germplasmName){
